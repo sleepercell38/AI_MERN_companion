23fa3bd5dca8789e38f3d483e52292f7ccc335b1
sleepercell38 <insanevishal07@gmail.com>
2025-06-09 18:39:33 +0530

    mongo atlas used for deployment

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB =  () => {
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+    mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
         console.log("database connected");
     }).catch((err)=>{
         console.log(err)
