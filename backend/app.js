d96b63f88e5b3132fbcb2d2517857d0d8b28e1c1
sleepercell38 <insanevishal07@gmail.com>
2025-06-09 17:47:24 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
 app.use("/users",userRoutes)
 app.use("/projects", projectroutes);
